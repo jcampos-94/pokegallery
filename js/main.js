795194c0e92d11b91349cb245dbe0fe5804117f8
jcampos-94 <jcampos.94@hotmail.com>
2024-10-19 19:02:58 -0500

    Sort by region and basic CSS added.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 import { renderListWithTemplate } from "./pokemonListing.mjs";
 
-renderListWithTemplate();
+// Add event listener for the region selection dropdown
+
+const regionDropdown = document.querySelector("#selectRegion");
+
+regionDropdown.addEventListener("change", (event) => {
+    const selectedRegion = event.target.value;
+
+    // Reder the list
+    if (selectedRegion) {
+        renderListWithTemplate(selectedRegion)
+    }
+});
