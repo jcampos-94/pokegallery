5087f79f6ec095679ebdd1c112251f1ed58fc235
jcampos-94 <jcampos.94@hotmail.com>
2024-10-24 13:49:37 -0500

    removed partials due to GitHub errors

--- js/detailed.js
+++ js/detailed.js
@@ -1,7 +1,3 @@
-import { loadHeaderFooter } from "./utils.mjs";
 import { renderDetailsWithTemplate } from "./detailListing.mjs";
-
-// Load Header
-loadHeaderFooter();
 
 renderDetailsWithTemplate();
