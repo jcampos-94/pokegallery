3931532e7a47f7f9d83c8f8546a081ddfa50d82e
jcampos-94 <jcampos.94@hotmail.com>
2024-10-24 13:44:53 -0500

    Revert "data rendered"

--- js/detailListing.mjs
+++ js/detailListing.mjs
@@ -1,32 +1,14 @@
 // Imports
-import { getParams, fetchData, formatPokemonName, getPropertyString, getStatsList, getTypesAsImages } from "./utils.mjs";
+import { getParams, fetchData, formatPokemonName } from "./utils.mjs";
 
 // Define AbortController for future uses
 let currentAbortController = null;
 
 // Template for the Pokemon details
-async function pokemonDetailsTemplate(entity, speciesData) {
-    return `<section class="species">
-        <h1 id="species-name">${formatPokemonName(entity.species.name)}</h1>
-        <h1 id="species-number">Nº ${speciesData.id.toString().padStart(4,"0")}</h1>
-    </section>
-    <div class"pokemon-data">
-        <section class="pokemon-form">
-            <h2>${formatPokemonName(entity.name)}</h2>
-            <img id="pokemon-img" src=${entity.sprites.other["official-artwork"].front_default}>
-            <div class="types-images">${await getTypesAsImages(entity)}</div>
-        </section>
-        <section>
-            <div class="pokemon-info">
-                <p><b>Height:</b> ${entity.height / 10} m</p>
-                <p><b>Weight:</b> ${entity.weight / 10} kg</p>
-                <p><b>Abilities:</b> ${getPropertyString(entity, "abilities", "ability")}</p>
-            </div>
-            <div class="pokemon-stats">
-                <p><b>Base Stats:</b>
-                <ul>${getStatsList(entity)}</ul>
-            </div>
-        </section>
+function pokemonDetailsTemplate(entity) {
+    return `<div class="pokemon">
+        <h2>${formatPokemonName(entity.species.name)}</h2>
+        <img src=${entity.sprites.other["official-artwork"].front_default}>
     </div>`
 }
 
@@ -54,7 +36,7 @@ export async function renderDetailsWithTemplate() {
         // Get entity Data
         const entityUrl = `${species.varieties[form].pokemon.url}`;
         const entity = await fetchData(entityUrl, signal);
-        const entityData = await pokemonDetailsTemplate(entity, species);
+        const entityData = pokemonDetailsTemplate(entity);
         detailsElement.innerHTML = entityData;
 
         } catch (error) {
